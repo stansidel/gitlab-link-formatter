2bf077ee5addc33ca1094eefe79c9ca1f2d18209
Сидельников Станислав <stanislav.k.sidelnikov@gazprombank.ru>
2024-09-13 13:15:36 +0500

    Initial Commit

---
+++ GitLab Link Formatter/GitLab_Link_FormatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLab_Link_FormatterApp.swift
+//  GitLab Link Formatter
+//
+//  Created by Stan Sidel Work on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct GitLab_Link_FormatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
